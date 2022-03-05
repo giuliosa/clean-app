ce61e4da97b702fe44db505ede42eccc45afed91
Giulio Sá <giuliocaetano@yahoo.com.br>
2022-03-05 17:20:42 -0300

    Initial topbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
+import Topbar from '../components/topbar'
 
 export default function Home() {
   return (
     <Layout>
       <Head>
         <title>{siteTitle}</title>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Rubik&display=swap');
+        </style> 
       </Head>      
-      <h1>{siteTitle}</h1>
+      <Topbar />
     </Layout>
   )
 }
